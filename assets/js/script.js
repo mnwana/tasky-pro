8c0363a04e395d5711a2483dfee563035febab4f
mnwana <mariellenwana@gmail.com>
2022-07-12 22:43:29 -0400

    add color gradient to btutons

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,7 +118,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -155,9 +155,11 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function (event) {
     $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
     $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
     $(event.target).addClass("dropover-active");
@@ -188,9 +190,14 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
-  over: function (event, ui) {},
-  out: function (event, ui) {},
+  over: function (event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  out: function (event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
 });
 
 $("#modalDueDate").datepicker({
